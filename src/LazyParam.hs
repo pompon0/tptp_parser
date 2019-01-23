b978f7df6eff1004d8ae5abaff76155183cceca4
Grzegorz Prusak <pompon.pompon@gmail.com>
2019-01-23 13:12:33 +0100

    Weak connection of LazyParam implemented

--- src/LazyParam.hs
+++ src/LazyParam.hs
@@ -97,6 +97,59 @@ lazyEq r s = do
   -- TODO: throw an error instead
   allM (map (\(s',v') -> pushAndCont weak (NegAtom (PEq s' v'))) (zip s v)) >>= return . join
 
+class Extract s where
+  extract :: s -> (s -> Term{-Var-} -> Term{-p-} -> M [()]) -> M [()]
+
+instance Extract s => Extract [s] where
+  extract [] cont = throw
+  extract (h:t) cont = anyM [extract t (cont.(h:)), extract h (cont.(:t))]
+
+instance Extract Term where
+  extract (TVar _) cont = throw
+  extract p@(TFun name args) cont = anyM [
+    extract args (cont.(TFun name)),
+    do { w <- allocVar; cont w w p }]
+
+instance Extract Pred where
+  extract (PEq l r) cont = anyM [
+    extract l (cont.(\l' -> PEq l' r)),
+    extract r (cont.PEq l)]
+  extract (PCustom name args) cont = extract args (cont.PCustom name) 
+
+instance Extract Atom where
+  extract (PosAtom p) cont = extract p (cont.PosAtom)
+  extract (NegAtom p) cont = extract p (cont.NegAtom)
+
+-- S || \Gamma, L[p], z~r
+-- S || \Gamma, L[p], f(s)~r
+strongLEq :: Atom -> (Term,Term) -> M [()]
+strongLEq aLp (l,r)= case l of
+  z@(TVar _) -> extract aLp (\aLw w p -> do {
+    addEQ (p,z);
+    addLT (w,z);
+    allM (map (pushAndCont weak) [aLw, NegAtom (PEq r w)]) >>= return.join
+  })
+  TFun f s -> extract aLp (\aLw w p -> do {
+    v <- mapM (\_ -> allocVar) s;
+    addEQ (p,TFun f v);
+    addLT (w,TFun f v);
+    let { subtasks = aLw : map (\(x,y) -> NegAtom (PEq x y)) (zip (r:s) (w:v)) };
+    allM (map (pushAndCont weak) subtasks) >>= return.join
+  })
+
+-- S || \Gamma, l~r, L[f(s)]
+strongEqL :: (Term,Term) -> Atom -> M [()]
+strongEqL (l,r) aLp = extract aLp (\aLw w p -> case p of
+  (TFun f s) -> do {
+    v <- mapM (\_ -> allocVar) s;
+    addEQ (TFun f v,l);
+    addLT (r,l);
+    addEQ (r,w);
+    let { subtasks = aLw : map (\(x,y) -> NegAtom (PEq x y)) (zip (r:s) (w:v)) };
+    allM (map (pushAndCont weak) subtasks) >>= return.join
+  }
+  _ -> throw)
+
 
 strong :: M [()]
 strong = do
@@ -104,33 +157,47 @@ strong = do
   case path of
     a:b:_ -> do
       case (a,b) of
+        -- S || \Gamma,!P[r],P[s]
+        -- S || \Gamma,P[r],!P[s]
         (NegAtom (PCustom n1 r), PosAtom (PCustom n2 s)) | n1 == n2 -> lazyEq r s
         (PosAtom (PCustom n1 r), NegAtom (PCustom n2 s)) | n1 == n2 -> lazyEq r s
         -- not sure if non-paramodulation strong step for equality predicate is needed
         -- TODO: verify that against the proof
-        (NegAtom (PEq r1 r2), PosAtom (PEq s1 s2)) -> lazyEq [r1,r2] [s1,s2]
-        (PosAtom (PEq r1 r2), NegAtom (PEq s1 s2)) -> lazyEq [r1,r2] [s1,s2]
-        (pL, PosAtom (PEq (TVar z) r)) -> error ""
-        (pL, PosAtom (PEq (TFun f a) r)) -> error ""
-        (PosAtom (PEq l r), pL) -> error ""
+        (NegAtom (PEq r1 r2), PosAtom (PEq s1 s2)) -> anyM [lazyEq [r1,r2] [s1,s2], lazyEq [r1,r2] [s2,s1]]
+        (PosAtom (PEq r1 r2), NegAtom (PEq s1 s2)) -> anyM [lazyEq [r1,r2] [s1,s2], lazyEq [r1,r2] [s2,s1]]
+        -- S || \Gamma, L[p], z~r
+        -- S || \Gamma, L[p], f(s)~r
+        (aLp, PosAtom (PEq l r)) -> anyM [strongLEq aLp (l,r), strongLEq aLp (r,l)]
+        -- S || \Gamma, l~r, L[f(s)]
+        (PosAtom (PEq l r), aLp) -> anyM [strongEqL (l,r) aLp, strongEqL (r,l) aLp]
         _ -> throw
     _ -> throw
 
+weakLEq :: Atom -> (Term,Term) -> M [()]
+weakLEq aLp (l,r) = extract aLp (\aLw w p -> do {
+  addEQ (p,l);
+  addLT (r,l);
+  addEQ (r,w);
+  pushAndCont weak aLw
+})
+
 weak :: M [()]
-weak = error ""
-
-{-
-===========================================
-
-addEQ :: (Term,Term) -> M ()
-addLT :: (Term,Term) -> M ()
-getPath :: M [Atom]
-
-============================================
-
-
-
-
-weak :: M ()
 weak = do
--}
+  BranchState path <- get
+  anyM [
+    -- S || \Gamma, s!~t
+    case path of { NegAtom (PEq l r):_ -> addEQ (l,r) >> return []; _ -> throw },
+    -- S || \Gamma L[p],\Delta,l~r
+    case path of { (PosAtom (PEq l r):t) -> anyM [weakLEq aLp s | s <- [(l,r),(r,l)], aLp <- t]; _ -> throw },
+    -- S || \Gamma l~r,\Delta,L[p]
+    case path of { (aLp:t) -> anyM [weakLEq aLp (l,r) | PosAtom (PEq l r) <- t]; _ -> throw },
+    -- S || \Gamma,!P[r],\Delta,P[s]
+    -- S || \Gamma,P[r],\Delta,!P[s]
+    case path of {
+      NegAtom (PCustom n1 s):t -> anyM [mapM addEQ (zip r s) >> return [] | PosAtom (PCustom n2 r) <- t, n1 == n2];
+      PosAtom (PCustom n1 s):t -> anyM [mapM addEQ (zip r s) >> return [] | NegAtom (PCustom n2 r) <- t, n1 == n2];
+      NegAtom (PEq l r):t -> anyM [mapM addEQ (zip [l2,r2] s) | s <- [[l,r],[r,l]], PosAtom (PEq l2 r2) <- t];
+      PosAtom (PEq l r):t -> anyM [mapM addEQ (zip [l2,r2] s) | s <- [[l,r],[r,l]], NegAtom (PEq l2 r2) <- t];
+      _ -> throw
+    }]
+
