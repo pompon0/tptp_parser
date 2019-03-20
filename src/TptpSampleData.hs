000790d37b3004143e34c49177799d82da36557b
Grzegorz Prusak <pompon.pompon@gmail.com>
2019-03-20 12:14:41 +0100

    added current benchmar outputs for LazyParam

--- src/TptpSampleData.hs
+++ src/TptpSampleData.hs
@@ -5,6 +5,7 @@ problems = [
   ("eqAxiom1",eqAxiom1),
   ("eqAxiom2",eqAxiom2),
   ("eqAxiom3",eqAxiom3),
+  ("eqAxiom4",eqAxiom4),
   ("barber",barber),
   ("pelletier20",pelletier20),
   ("pelletier24",pelletier24)]
@@ -14,6 +15,7 @@ simple = "fof(simple, conjecture, (?[X]:(![Y]: (p(X) => p(Y)))))."
 eqAxiom1 = "fof(eqAxiom1, conjecture, ((a1=a2) & (b1=b2) & (c1=c2)) => (f(a1,b1,c1)=f(a2,b2,c2)))."
 eqAxiom2 = "fof(eqAxiom2, conjecture, (a1=a2) => (f(a1,b1,c1)=f(a2,b1,c1)))."
 eqAxiom3 = "fof(eqAxiom3, conjecture, ((a=b) & (b=c) & (c=d)) => (a=d))."
+eqAxiom4 = "fof(eqAxiom4, conjecture, ((a=b)  => (b=a)))."
 
 barber = "fof(simple, conjecture, ~(?[B]:(![X]:(shaves(B,X) <=> ~shaves(X,X)))))."
 
